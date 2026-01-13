5d45c1ba1b2fe2633229546dc0e0b6829155a926
Lev <leviksun.s@gmail.com>
2026-01-13 15:08:36 +0300

    поправил pom и тесты

---
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -0,0 +1,48 @@
+package ru.yandex.practicum.filmorate;
+
+import model.Film;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.controller.FilmController;
+
+import java.time.Duration;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FilmControllerTest {
+    FilmController controller;
+    Film film;
+
+    @BeforeEach
+    public void setController(){
+        controller = new FilmController();
+        film = new Film();
+    }
+
+    @Test
+    public void addFilmTest(){
+       RuntimeException exception = assertThrows(RuntimeException.class, () -> controller.addFilm(film));
+       assertEquals(exception.getMessage(), "Укажите имя фильма");
+
+       film.setName("Форсаж");
+       exception = assertThrows(RuntimeException.class, () -> controller.addFilm(film));
+       assertEquals(exception.getMessage(), "Укажите описание фильма");
+
+       film.setDescription("Фильм форсаж");
+       exception = assertThrows(RuntimeException.class, () -> controller.addFilm(film));
+       assertEquals(exception.getMessage(), "Укажите дату релиза");
+
+       film.setReleaseDate(LocalDate.now());
+       exception = assertThrows(RuntimeException.class, () -> controller.addFilm(film));
+       assertEquals(exception.getMessage(), "Укажите продолжительность фильма");
+
+       film.setDuration(Duration.ofHours(3));
+
+       Film savedFilm = controller.addFilm(film);
+       assertNotNull(savedFilm.getId());
+       assertEquals(1, controller.getFilms().size());
+       assertEquals("Форсаж", savedFilm.getName());
+    }
+
+}
