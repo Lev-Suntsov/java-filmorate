1c7fbdd19f6586b52d0367140e7c465490e46be1
Lev <leviksun.s@gmail.com>
2026-03-16 22:06:04 +0300

    Финальный проект 12

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -1,20 +1,27 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import javassist.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.filmorate.exeptions.NotFoundException; // ← вот этот импорт
 
 import javax.xml.bind.ValidationException;
 import java.util.Map;
 
 @RestControllerAdvice
 public class ErrorHandler {
+
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Map<String, String> notFoundExceptionHandler(NotFoundException e) {
+        return Map.of("error", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> validateExeptionHandler(ValidationException e) {
         return Map.of("error", e.getMessage());
     }
 
@@ -25,11 +32,5 @@ public class ErrorHandler {
                 "message", e.getMessage()
         );
         return ResponseEntity.internalServerError().body(error);  // 500
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> validateExeptionHandler(ValidationException e) {
-        return Map.of("error", e.getMessage());
     }
 }
